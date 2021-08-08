b1124b6621aee52a22e65a34221486c0c5484665
AnnaKnol <83373222+AnnaKnol@users.noreply.github.com>
2021-08-08 16:06:40 +0200

    deleted icons from manifest.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+// import axios from "axios";
 import CurrentState from "./CurrentState";
 import CurrentTemperature from "./CurrentTemperature";
 import MaxAndMin from "./MaxAndMin";
